f505d8fae4087d189365342030f491f9afd9cadd
Patrick-Ranjit D. Madsen <prm@PatricknjitsMBP.lan>
2019-08-03 12:08:27 +0200

    add base AgileCRM client

---
+++ tap-agilecrm/agilecrm_client.py
@@ -0,0 +1,72 @@
+from requests import Session
+
+
+class AgileCRM:
+    def __init__(
+        self,
+        email,
+        domain,
+        api_key,
+        pagination_page_size=50,
+        pagination_global_sort_key="-created_time",
+        **kwargs,
+    ):
+        self.__session = Session(**kwargs)
+        self.__session.auth = (email, api_key)
+        self.__session.headers.update({"Accept": "application/json"})
+
+        self.pagination_page_size = pagination_page_size
+        self.pagination_global_sort_key = pagination_global_sort_key
+
+        self.base_url = f"https://{domain}.agilecrm.com/dev/api/"
+
+    def list_deals(self, **kwargs):
+        yield from self.paginate("GET", "opportunity", **kwargs)
+
+    def list_contacts(self, **kwargs):
+        yield from self.paginate("POST", "contacts/list", **kwargs)
+
+    def list_companies(self, **kwargs):
+        yield from self.paginate("POST", "contacts/companies/list", **kwargs)
+
+    def paginate(self, method, path, page_size=None, global_sort_key=None, **kwargs):
+        args = {
+            "page_size": page_size or self.pagination_page_size,
+            "global_sort_key": global_sort_key or self.pagination_global_sort_key,
+        }
+
+        if method == "GET":
+            kwargs["params"] = args
+        else:
+            kwargs["data"] = args
+
+        yield from self.__paginate(method, path, **kwargs)
+
+    def __paginate(self, method, path, data=None, params=None, **kwargs):
+        """paginates over items with a trailing item containing a cursor and passes arguments as either 'data' or 'params' depending on method"""
+        while True:
+            items = self.request(method, path, data=data, params=params, **kwargs)
+
+            if not items:
+                return
+
+            # remove possible 'cursor' value from last item
+            cursor = items[-1].pop("cursor", None)
+
+            for item in items:
+                yield item
+
+            if not cursor:
+                return
+
+            if data:
+                data["cursor"] = cursor
+            elif params:
+                params["cursor"] = cursor
+
+    def request(self, method, path, **kwargs):
+        resp = self.__session.request(method, self.base_url + path, **kwargs)
+        if resp:
+            return resp.json()
+
+        resp.raise_for_status()
