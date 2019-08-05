6da70e9ff40ab97502f8ae21c8c016c9c3f83c91
Patrick-Ranjit D. Madsen <prm@Patrick-Ranjits-MacBook-Pro.local>
2019-08-05 15:43:12 +0200

    filter unwanted keys in payloads

--- tap-agilecrm/__init__.py
+++ tap-agilecrm/__init__.py
@@ -4,7 +4,7 @@ import json
 import os
 import sys
 from datetime import datetime
-from typing import Any, Dict, Generator, List
+from typing import Any, Dict, Generator, List, Set
 
 import singer
 from singer import utils
@@ -30,27 +30,109 @@ def main():
     process_deals()
 
 def process_companies(**kwargs):
+    fields = {
+        "created_time",
+        "entity_type",
+        "company_entity",
+        "id",
+        "is_lead_converted",
+        "klout_score",
+        "last_called",
+        "last_campaign_emaild",
+        "last_contacted",
+        "last_emailed",
+        "lead_converted_time",
+        "lead_score",
+        "lead_source_id",
+        "lead_status_id",
+        "owner",
+        "properties",
+        "star_value",
+        "tags",
+        "trashed_time",
+        "type",
+        "updated_time",
+        "viewed_time",
+    }
+    
     process_stream(
         stream_name = "company",
         stream_generator = client.list_companies(**kwargs),
         key_properties = ["id"],
         bookmark_properties = ["updated_time"],
+        include_fields=fields,
     )
 
 def process_contacts(**kwargs):
+    fields = {
+        "contact_company_id",
+        "created_time",
+        "entity_type",
+        "id",
+        "is_lead_converted",
+        "klout_score",
+        "last_called",
+        "last_campaign_emaild",
+        "last_contacted",
+        "last_emailed",
+        "lead_converted_time",
+        "lead_score",
+        "lead_source_id",
+        "lead_status_id",
+        "owner",
+        "properties",
+        "source",
+        "star_value",
+        "tags",
+        "type",
+        "updated_time",
+        "viewed_time",
+    }
     process_stream(
         stream_name = "contact",
         stream_generator = client.list_contacts(**kwargs),
         key_properties = ["id"],
         bookmark_properties = ["updated_time"],
+        include_fields=fields,
     )
 
 def process_deals(**kwargs):
+    fields = {
+        "apply_discount",
+        "archived",
+        "close_date",
+        "colorName",
+        "contact_ids",
+        "contacts",
+        "created_time",
+        "currency_conversion_value",
+        "custom_data",
+        "deal_source_id",
+        "discount_amt",
+        "discount_type",
+        "discount_value",
+        "entity_type",
+        "expected_value",
+        "id",
+        "isCurrencyUpdateRequired",
+        "lost_reason_id",
+        "milestone",
+        "milestone_changed_time",
+        "name",
+        "pipeline_id",
+        "probability",
+        "products",
+        "tags",
+        "total_deal_value",
+        "updated_time",
+    }
+
     process_stream(
         stream_name = "deal",
         stream_generator = client.list_deals(**kwargs),
         key_properties = ["id"],
         bookmark_properties = ["updated_time"],
+        include_fields=fields,
     )
 
 
@@ -60,6 +142,7 @@ def process_stream(
     key_properties: List[str],
     bookmark_properties: str = None,
     stream_alias: str = None,
+    include_fields: Set[str] = None,
 ):
     # load schema from disk
     with open(f"schemas/{stream_name}_schema.json", "r") as fp:
@@ -72,8 +155,32 @@ def process_stream(
     # instrument with metrics to allow consumers to receive progress
     with singer.metrics.record_counter(stream_name or stream_alias) as counter:
         for record in stream_generator:
+            if include_fields:
+                keys = list(record.keys())
+                for key in keys:
+                    if key in include_fields:
+                        continue
+                    record.pop(key)
+
             singer.write_record(stream_name, record, time_extracted=utils.now())
+
             counter.increment(1)
+
+
+def write_record(record, include_fields):
+    entity_type = record["entity_type"]
+    with open(f"schemas/{entity_type}_payload.json", "w") as fp:
+        json.dump(record, fp, sort_keys=True, indent="  ")
+    
+    keys = list(record.keys())
+    for key in keys:
+        if key in include_fields:
+            continue
+        record.pop(key)
+    
+    with open(f"schemas/{entity_type}_filtered.json", "w") as fp:
+        json.dump(record, fp, sort_keys=True, indent="  ")
+
 
 if __name__ == "__main__":
     main()
