2c71f681b8908fa06f8bab9703fd6c0a69c3f387
Patrick-Ranjit D. Madsen <prm@Patrick-Ranjits-MacBook-Pro.local>
2019-08-06 11:02:13 +0200

    do not filter any fields just yet

--- tap-agilecrm/__init__.py
+++ tap-agilecrm/__init__.py
@@ -30,109 +30,27 @@ def main():
     process_deals()
 
 def process_companies(**kwargs):
-    fields = {
-        "created_time",
-        "entity_type",
-        "company_entity",
-        "id",
-        "is_lead_converted",
-        "klout_score",
-        "last_called",
-        "last_campaign_emaild",
-        "last_contacted",
-        "last_emailed",
-        "lead_converted_time",
-        "lead_score",
-        "lead_source_id",
-        "lead_status_id",
-        "owner",
-        "properties",
-        "star_value",
-        "tags",
-        "trashed_time",
-        "type",
-        "updated_time",
-        "viewed_time",
-    }
-    
     process_stream(
         stream_name = "company_entity",
         stream_generator = client.list_companies(**kwargs),
         key_properties = ["id"],
         bookmark_properties = ["updated_time"],
-        include_fields=fields,
     )
 
 def process_contacts(**kwargs):
-    fields = {
-        "contact_company_id",
-        "created_time",
-        "entity_type",
-        "id",
-        "is_lead_converted",
-        "klout_score",
-        "last_called",
-        "last_campaign_emaild",
-        "last_contacted",
-        "last_emailed",
-        "lead_converted_time",
-        "lead_score",
-        "lead_source_id",
-        "lead_status_id",
-        "owner",
-        "properties",
-        "source",
-        "star_value",
-        "tags",
-        "type",
-        "updated_time",
-        "viewed_time",
-    }
     process_stream(
         stream_name = "contact_entity",
         stream_generator = client.list_contacts(**kwargs),
         key_properties = ["id"],
         bookmark_properties = ["updated_time"],
-        include_fields=fields,
     )
 
 def process_deals(**kwargs):
-    fields = {
-        "apply_discount",
-        "archived",
-        "close_date",
-        "colorName",
-        "contact_ids",
-        "contacts",
-        "created_time",
-        "currency_conversion_value",
-        "custom_data",
-        "deal_source_id",
-        "discount_amt",
-        "discount_type",
-        "discount_value",
-        "entity_type",
-        "expected_value",
-        "id",
-        "isCurrencyUpdateRequired",
-        "lost_reason_id",
-        "milestone",
-        "milestone_changed_time",
-        "name",
-        "pipeline_id",
-        "probability",
-        "products",
-        "tags",
-        "total_deal_value",
-        "updated_time",
-    }
-
     process_stream(
         stream_name = "deal", # no '_entity' suffix here
         stream_generator = client.list_deals(**kwargs),
         key_properties = ["id"],
         bookmark_properties = ["updated_time"],
-        include_fields=fields,
     )
 
 
@@ -145,8 +63,7 @@ def process_stream(
     include_fields: Set[str] = None,
 ):
     # load schema from disk
-    with open(f"schemas/{stream_name}_schema.json", "r") as fp:
-        schema = json.load(fp)
+    schema = load_schema(stream_name)
 
     # write schema
     singer.write_schema(stream_name, schema, key_properties, stream_alias)
@@ -166,6 +83,9 @@ def process_stream(
 
             counter.increment(1)
 
+def load_schema(stream_name):
+    with open(f"schemas/{stream_name}_schema.json", "r") as fp:
+        return json.load(fp)
 
 def write_record(record, include_fields):
     entity_type = record["entity_type"]
