1ad8fdd76a5c1f10bcc58342a9bbe785d4827a85
Patrick-Ranjit D. Madsen <prm@patricknjitsmbp.lan>
2019-08-21 15:27:10 +0200

    fix relative import

--- tap_agilecrm/__init__.py
+++ tap_agilecrm/__init__.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import singer
 from singer import utils
 
-from agilecrm_client import AgileCRM
+from tap_agilecrm.agilecrm_client import AgileCRM
 
 logger = singer.get_logger()
 
