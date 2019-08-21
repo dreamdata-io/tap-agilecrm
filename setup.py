470052040e4809b18e85f1a91e8b96933c70fbb9
Patrick-Ranjit D. Madsen <prm@patricknjitsmbp.lan>
2019-08-21 15:45:18 +0200

    black commit

--- setup.py
+++ setup.py
@@ -8,21 +8,20 @@ setup(
     description="Singer.io tap for extracting data AgileCMS",
     author="Dreamdata",
     url="https://dreamdata.io",
-    py_modules=['tap-agilecrm'],
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     install_requires=["singer-python==5.8.0", "requests==2.22.0"],
     entry_points="""
-          [console_scripts]
-          tap-agilecrm=tap_agilecrm:main
-      """,
+        [console_scripts]
+        tap-agilecrm=tap_agilecrm:main
+    """,
     include_package_data=True,
-    package_data = {
-          'schemas': [
-              "company_schema_infer.json",
-              "contact_schema_infer.json",
-              "deal_schema_infer.json",
-          ]
-      },
+    package_data={
+        "schemas": [
+            "company_schema_infer.json",
+            "contact_schema_infer.json",
+            "deal_schema_infer.json",
+        ]
+    },
     packages=["tap_agilecrm"],
 )
 
