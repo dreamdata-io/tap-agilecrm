c9d678038a11d0a4f95270321939ad45020670bd
Adam <przywarty.it@gmail.com>
2020-07-22 14:33:33 +0200

    Get config only from file

--- tap_agilecrm/__init__.py
+++ tap_agilecrm/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import json
-import os
 import sys
 from datetime import datetime
 from typing import Dict, Any, Optional, Callable
@@ -15,16 +14,11 @@ from tap_agilecrm.streams import process_streams
 logger = singer.get_logger()
 
 
-AGILECRM_EMAIL = "AGILECRM_EMAIL"
-AGILECRM_DOMAIN = "AGILECRM_DOMAIN"
-AGILECRM_API_KEY = "AGILECRM_API_KEY"
-
-
 def main():
     args = utils.parse_args(["config"])
-    EMAIL = args.config.get("email") or os.environ.get(AGILECRM_EMAIL)
-    DOMAIN = args.config.get("domain") or os.environ.get(AGILECRM_DOMAIN)
-    API_KEY = args.config.get("api_key") or os.environ.get(AGILECRM_API_KEY)
+    EMAIL = args.config.get("email")
+    DOMAIN = args.config.get("domain")
+    API_KEY = args.config.get("api_key")
 
     client = AgileCRM(EMAIL, DOMAIN, API_KEY)
 
